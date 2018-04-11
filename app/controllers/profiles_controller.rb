74528bef398879ae7f57a58ce5eaef65906937d2
greenlakes <greenlakes2005@gmail.com>
2018-04-11 16:22:52 +0000

    final draft

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+   before_action :only_current_user
+  
   # GET to /users/:user_id/profile/new
   def new
     @profile = Profile.new
@@ -18,10 +21,32 @@ def create
   end
 end
 
+def edit
+  @user = User.find(params[:user_id])
+  @profile = @user.profile
+end
+
+def update
+  @user = User.find(params[:user_id])
+  @profile = @user.profile
+  
+  if @profile.update_attributes(profile_params)
+    flash[:success] = "Profile has been updated!"
+    redirect_to user_path(params[:user_id])
+  else
+    render action: :edit
+  end
+end
+
 private
   
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+  end
+  
+  def only_current_user
+    @user = User.find(params[:user_id])
+    redirect_to(root_path) unless @user == current_user
   end
   
 end
